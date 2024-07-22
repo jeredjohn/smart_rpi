6982849aada150ee61117fe439f55e074c1e3f9f
Jered Butler <jered.john.butler@gmail.com>
2024-07-22 15:01:04 -0500

    Ready to test Gpio

--- app.py
+++ app.py
@@ -32,65 +32,66 @@ def service_worker():
 def devices():
     return render_template('devices.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
+'''
+	Raspberry Pi GPIO Status and Control
+'''
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   23 : {'name' : 'GPIO 23', 'state' : GPIO.LOW},
-   24 : {'name' : 'GPIO 24', 'state' : GPIO.LOW}
-   }
-
-# Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
-
-@app.route("/lamp")
-def lamp():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
-   templateData = {
-      'pins' : pins
-      }
-   # Pass the template data into the template main.html and return it to the user
-   return render_template('lamp.html', **templateData)
-
-# The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/lamp/<changePin>/<action>")
-def action(changePin, action):
-   # Convert the pin from the URL into an integer:
-   changePin = int(changePin)
-   # Get the device name for the pin being changed:
-   deviceName = pins[changePin]['name']
-   # If the action part of the URL is "on," execute the code indented below:
-   if action == "on":
-      # Set the pin high:
-      GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
-   if action == "off":
-      GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
-
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'pins' : pins
-   }
-
-   return render_template('lamp.html', **templateData)
-
-
-
+GPIO.setwarnings(False)
+#define actuators GPIOs
+ledRed = 23
+ledYlw = 24
+ledGrn = 17
+#initialize GPIO status variables
+ledRedSts = 0
+ledYlwSts = 0
+ledGrnSts = 0
+# Define led pins as output
+GPIO.setup(ledRed, GPIO.OUT)   
+GPIO.setup(ledYlw, GPIO.OUT) 
+GPIO.setup(ledGrn, GPIO.OUT) 
+# turn leds OFF 
+GPIO.output(ledRed, GPIO.LOW)
+GPIO.output(ledYlw, GPIO.LOW)
+GPIO.output(ledGrn, GPIO.LOW)
+	
+@app.route("/gpio")
+def gpio():
+	# Read Sensors Status
+	ledRedSts = GPIO.input(ledRed)
+	ledYlwSts = GPIO.input(ledYlw)
+	ledGrnSts = GPIO.input(ledGrn)
+	templateData = {
+              'title' : 'GPIO output Status!',
+              'ledRed'  : ledRedSts,
+              'ledYlw'  : ledYlwSts,
+              'ledGrn'  : ledGrnSts,
+        }
+	return render_template('gpio.html', **templateData)
+	
+@app.route("/<deviceName>/<action>")
+def action(deviceName, action):
+	if deviceName == 'ledRed':
+		actuator = ledRed
+	if deviceName == 'ledYlw':
+		actuator = ledYlw
+	if deviceName == 'ledGrn':
+		actuator = ledGrn
+   
+	if action == "on":
+		GPIO.output(actuator, GPIO.HIGH)
+	if action == "off":
+		GPIO.output(actuator, GPIO.LOW)
+		     
+	ledRedSts = GPIO.input(ledRed)
+	ledYlwSts = GPIO.input(ledYlw)
+	ledGrnSts = GPIO.input(ledGrn)
+   
+	templateData = {
+              'ledRed'  : ledRedSts,
+              'ledYlw'  : ledYlwSts,
+              'ledGrn'  : ledGrnSts,
+	}
+	return render_template('gpio.html', **templateData)
 
 
